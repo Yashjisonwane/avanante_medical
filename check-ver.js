c3c14e146d5ca48b9329c04dda94d415535ca84b
mahajanvishal123 <mahajanvishal9644@gmail.com>
2026-05-06 15:58:31 +0530

    first commit

---
+++ check-ver.js
@@ -0,0 +1,4 @@
+var p = require('./node_modules/expo/package.json');
+console.log('Installed expo:', p.version);
+var rn = require('./node_modules/react-native/package.json');
+console.log('Installed react-native:', rn.version);
